c9be732f6504779ee48fbaa5d32e2a822a5c38ee
Sean Yeatts <39303151+SeanYeatts@users.noreply.github.com>
2025-02-01 20:20:08 -0500

    Add files via upload

--- swiftstorage/__init__.py
+++ swiftstorage/__init__.py
@@ -9,7 +9,7 @@ A simple paradigm for local and remote file manipulation.
 
 from __future__ import annotations
 
-__title__       = "easystorage"
+__title__       = "swiftstorage"
 __author__      = "Sean Yeatts"
 __copyright__   = "Copyright (c) 2025 Sean Yeatts. All rights reserved."
 
